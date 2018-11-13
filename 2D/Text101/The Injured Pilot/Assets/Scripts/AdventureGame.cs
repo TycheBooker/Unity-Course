2fe75473ced7ef651dd6c3ecd7d86e6cde92135d
TycheBooker <tihana.bihler@gmail.com>
2018-11-13 11:11:39 +0100

    new project structure

---
+++ 2D/Text101/The Injured Pilot/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Use this for initialization
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            state = nextStates[0];
+        } 
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+        textComponent.text = state.GetStateStory();
+    }
+}

72ad91778b862d8dbf7d5d420b12ee62b7727857
TycheBooker <tihana.bihler@gmail.com>
2018-11-13 16:41:10 +0100

    Number Wizard

---
+++ 2D/Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour {
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+    // Use this for initialization
+    void Start () {
+        StartGame();
+    }
+
+    void StartGame() {
+        NextGuess();
+    }
+
+    public void OnPressHigher() {
+        if (min < guess) {
+            min = guess + 1;
+        }
+        NextGuess();
+    }
+
+    public void OnPressLower() {
+        if (max > guess) {
+            max = guess -1;
+        }
+        NextGuess();
+    }
+
+    void NextGuess() {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+}
--- 2D/Text101/The Injured Pilot/Assets/Scripts/AdventureGame.cs
+++ 2D/Text101/The Injured Pilot/Assets/Scripts/AdventureGame.cs
@@ -28,16 +28,13 @@ public class AdventureGame : MonoBehaviour
     private void ManageState()
     {
         var nextStates = state.GetNextStates();
-        if (Input.GetKeyDown(KeyCode.Alpha1)) {
-            state = nextStates[0];
-        } 
-        else if (Input.GetKeyDown(KeyCode.Alpha2)) {
-            state = nextStates[1];
+
+        for (int i = 0; i < nextStates.Length; i++) {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i)) {
+                state = nextStates[i];
+            } 
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            state = nextStates[2];
-        }
+
         textComponent.text = state.GetStateStory();
     }
 }
